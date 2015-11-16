9abbb589581a34ea0c59a7cf90b8dd2c430e4a1e
rockdai <rockdai@qq.com>
2015-11-16 22:06:54 +0800

    fix cookie

--- crawler.js
+++ crawler.js
@@ -12,12 +12,13 @@ const openId = 'oIWsFt3dWfYvPlfc8ZBBzjvffl8Q';
 const ext = 'h3pzFjLZCZ0Y0nmoc3y7fQ8SxOv38L2PAEXyLuthQkRPIOQWPZOnzRgITve0Ud48';
 const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36';
 const mockHeaders = {
-  'Cookie': 'ABTEST=7|1447681194|v1; SUID=054B782A260C930A000000005649DCAA; SUID=054B782A3108990A000000005649DCAA; SUV=001610022A784B055649DCAAC2ABA204; weixinIndexVisited=1; sct=1; SNUID=E9A794C1ECE9CF43674D9F7EEC97EC4D; IPLOC=CN3100; wapsogou_qq_nickname=',
+  'Cookie': 'CXID=870C18F2A7F60593504B09F2D642299E; SUID=9806D771556C860A56469ABC00081AC0; ad=Qyllllllll2QSI$DlllllVBp7wwllllltNtpullllxtlllllROxlw@@@@@@@@@@@; ABTEST=0|1447655412|v1; SUV=005678392A784B02564977F4AC1FE321; weixinIndexVisited=1; SNUID=175F6C3E151030B79E467A35158C8D67; ppinf=5|1447661995|1448871595|Y2xpZW50aWQ6NDoyMDE3fGNydDoxMDoxNDQ3NjYxOTk1fHJlZm5pY2s6NzpyaWNoYXJkfHRydXN0OjE6MXx1c2VyaWQ6NDQ6MDdDMTk5NUE1RjY2OEI3MDdCM0Q3MjhBRjk0QjhDNjFAcXEuc29odS5jb218dW5pcW5hbWU6NzpyaWNoYXJkfA; pprdig=vYoosRiYsly_dG9xF4QZKwieMbWcY4PgXc3JzrBb-fUNhpLIdXnOovlLaFQ80Wh5daXoxUH0xefLMdjdh_85C8BuoQ_XrEnJljk7xdQu04xXmX0Bjj_GVxZ-tzPpNWCHnq_8KitC386i9AZRDuYWHLnIRN-2wi_aJln7Tz9Plog; PHPSESSID=ag3qu15q3s69vnv1kdl4rbpnl4; SUIR=175F6C3E151030B79E467A35158C8D67; ppmdig=1447681981000000154dc1b9b56d4530abda30e346f3be48; sct=7; wapsogou_qq_nickname=; IPLOC=CN3100',
   'Host': 'weixin.sogou.com',
   'User-Agent': userAgent,
 };
 let skipPage = 0;
 let totalPage = 10;
+let interval = 60; // 60s
 
 function onerror(err) {
   console.log(err);
@@ -91,7 +92,7 @@ function handleList(res) {
     let articleTitle = node[2].val.trim();
     let articleLink = node[3].val.trim();
     handleArticle(articleLink, articleTitle);
-    sleep(60);
+    sleep(interval);
   }
 }
 
@@ -107,7 +108,7 @@ if (!fs.existsSync(outputDir)) {
 
 for (let page = 1 + skipPage; page <= totalPage; page++) {
   requestList(page);
-  sleep(60);
+  sleep(interval);
 }
 
 
